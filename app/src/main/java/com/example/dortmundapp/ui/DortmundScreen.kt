ca8177c02bc5d31b3a2cdc1055b52ee7441d36a4
Laura Voetzsch <56238822+Laurax64@users.noreply.github.com>
2023-08-07 17:55:00 +0200

    Add DortmundScreen.kt

---
+++ app/src/main/java/com/example/dortmundapp/ui/DortmundScreen.kt
@@ -0,0 +1,19 @@
+package com.example.dortmundapp.ui
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+/**
+ * Main composable that serves as container which displays
+ * content according to [DortmundViewModel.uiState] and [windowSize]
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DortmundApp(windowSize: WindowWidthSizeClass) {
+    val viewModel: DortmundViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
+}
